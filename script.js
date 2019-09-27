89fa11fa948bf806b33386eccee2971f74418126
Andres Delgado <adelgado0723@gmail.com>
2019-09-27 00:29:23 -0400

    fixed paths for Linux

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let configPath = "";
 
 switch (process.env.NODE_ENV) {
   case "production":
-    configPath = path.join(__dirname, "\\configs\\prod\\.env");
+    configPath = path.join(__dirname, "/configs/prod/.env");
     break;
 
   case "development":
-    configPath = path.join(__dirname, "\\configs\\dev\\.env");
+    configPath = path.join(__dirname, "/configs/dev/.env");
     break;
 
   default:
@@ -15,7 +15,9 @@ switch (process.env.NODE_ENV) {
     return;
 }
 
-require("dotenv").config({ path: configPath });
+require("dotenv").config({
+  path: configPath
+});
 
 const RSAUtilityGenerator = require("./RSAUtilityGenerator");
 const utils = RSAUtilityGenerator();
@@ -23,13 +25,13 @@ const utils = RSAUtilityGenerator();
 utils.generateKeys(process.env.KEY_FOLDER_PATH);
 
 utils.encrypt(
-  process.env.PUBLIC_KEY_PATH,
+  path.normalize(process.env.PUBLIC_KEY_PATH),
   path.normalize("./data/test.txt"),
   path.normalize("./test/encrypted.txt")
 );
 
 utils.decrypt(
-  process.env.PRIVATE_KEY_PATH,
+  path.normalize(process.env.PRIVATE_KEY_PATH),
   path.normalize("./test/encrypted.txt"),
   path.normalize("./test/decrypted.txt")
 );
